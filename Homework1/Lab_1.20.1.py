0d895705c3148e13a19fafcee7ca91786ac98c79
MICHAEL DONALD <35203158+Nitercrawler@users.noreply.github.com>
2021-02-06 20:23:06 -0600

    Add files via upload

---
+++ Homework1/Lab_1.20.1.py
@@ -0,0 +1,13 @@
+# Michael Donald
+# 1896510
+
+user_num = int(input('Enter integer:\n'))  # The first integer
+squared = user_num * user_num  # Squared integer
+cubed = squared * user_num  # Cube output
+print('You entered:', user_num)  # Print integer
+print(user_num, 'squared is', squared)  # print user_num squared
+print('And', user_num, 'cubed is', cubed, '!!')  # Cube user_num
+user_num2 = int(input('Enter another integer:\n'))  # Input another integer
+print(user_num, '+', user_num2, 'is', user_num + user_num2)  # print 4 + 5 is 9
+print(user_num, '*', user_num2, 'is', user_num * user_num2)  # print 4 * 5 is 20
+
