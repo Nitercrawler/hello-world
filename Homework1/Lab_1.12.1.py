5c7441833b7ca75e82aa3320ba9ffc9e2e4670e8
MICHAEL DONALD <35203158+Nitercrawler@users.noreply.github.com>
2021-02-06 20:22:40 -0600

    Add files via upload

---
+++ Homework1/Lab_1.12.1.py
@@ -0,0 +1,8 @@
+# Michael Donald
+# 1896510
+
+userNum = int(input())
+userNumSquared = userNum * userNum  # Bug here
+
+print(userNumSquared)  # Output formatting issue here; fix it when instructed
+
