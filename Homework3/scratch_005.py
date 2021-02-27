f1465b1ce825c1b4d424f9377ab52e903b54966c
MICHAEL DONALD <35203158+Nitercrawler@users.noreply.github.com>
2021-02-27 14:43:42 -0600

    Add files via upload

---
+++ Homework3/scratch_005.py
@@ -0,0 +1,16 @@
+# Michael Donald # 1896510 #
+# College of Technology
+# The University of Houston#
+# CIS 2348 -    #
+# 2.12.1 LAB
+# user defined input
+user_num = int(input())
+# user defined input
+x = int(input())
+# loop the code for three times so as to divide the user_num with x three times
+for i in range(3):
+    # divide user_num either
+    user_num = user_num // x
+    # print the value
+    print(user_num, end='')
+# input 2000, 2
